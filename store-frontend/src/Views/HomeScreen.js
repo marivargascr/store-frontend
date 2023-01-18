540c37cfcf9689bff29fb60eab5ce53cb329e129
marivargascr <mvb1996.23@gmail.com>
2023-01-18 14:22:57 +0100

    actions and components
    This includes components such as
    message,
    loader
    goal,
    paginate
    products
    
    and
    actions like:
    cart actions
    order Actions
    Product Actions
    User Actions

--- store-frontend/src/Views/HomeScreen.js
+++ store-frontend/src/Views/HomeScreen.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from "react-bootstrap";
 import Product from '../components/Product'
-import products from '../products';
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+  
+
+  const productList = useSelector((state) => state.productList)
+
+  console.log(productList);
+
+  const { loading, error, products } = productList
+
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row> 
-      {products.map((product) => (
-     <Col key ={product._id} sm={12} md={6} lg={4} xl={13}>
-       <Product product={product}></Product>
-     </Col> 
-      ))}
-      </Row>
+      {loading ? (
+         <h2> Loading...</h2> 
+         ) :  error ? (
+         <h3>{error}</h3> 
+         ) : ( 
+         <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={13}>
+            <Product product={product}/>
+          </Col>
+        ))}
+      </Row>)}
     </>
-  );
+  )
 }
+
+
 
 export default HomeScreen;
