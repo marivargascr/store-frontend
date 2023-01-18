46bf14b55543e9a25906e0b9f6229ac3c4d24473
marivargascr <mvb1996.23@gmail.com>
2023-01-18 12:30:49 +0100

    constants and redux

--- store-frontend/src/index.js
+++ store-frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import  {Provider} from 'react-redux';
+import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './css/bootstrap.min.css';
@@ -7,9 +9,10 @@ import './css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Provider store={store}>
+<App />
+</Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
